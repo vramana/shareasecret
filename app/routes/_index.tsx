6717bd74f72b405f72b7d957b6498deee9be1c38
Sarvani Sanaboyina <sarvani.sanaboyina@gmail.com>
2024-08-16 00:00:41 +0530

    changes

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,4 +1,8 @@
 import type { MetaFunction } from "@remix-run/node";
+import type { ActionFunctionArgs } from "@remix-run/node";
+import { Form, useActionData } from "@remix-run/react";
+import { encrypt, decrypt } from "../../server/crpytion.server";
+import { json } from "@remix-run/node";
 
 export const meta: MetaFunction = () => {
   return [
@@ -7,42 +11,73 @@ export const meta: MetaFunction = () => {
   ];
 };
 
+export async function action({ request }: ActionFunctionArgs) {
+  const formData = await request.formData();
+  const intent = formData.get("intent");
+
+  switch (intent) {
+    case "encrypt":
+      const text = String(formData.get("text"));
+      const key = await encrypt(text);
+      return json({ success: true, key: key });
+
+    case "decrypt":
+      const userKey = String(formData.get("key"));
+
+      const message = await decrypt(userKey);
+      return json({ success: true, message });
+
+    default:
+      return json({ success: false, message: "Unknown action" });
+  }
+}
+
 export default function Index() {
+  const data = useActionData<typeof action>();
+  console.log({ data });
+
   return (
-    <div className="font-sans p-4">
-      <h1 className="text-3xl">Welcome to Remix</h1>
-      <ul className="list-disc mt-4 pl-6 space-y-2">
-        <li>
-          <a
-            className="text-blue-700 underline visited:text-purple-900"
-            target="_blank"
-            href="https://remix.run/start/quickstart"
-            rel="noreferrer"
+    <div className="text-center py-10">
+      <div>
+        <h4 className="text-gray-900 py-6 font-bold text-5xl">
+          Enter your secret here
+        </h4>
+        <Form method="post">
+          <input type="hidden" name="intent" value="encrypt" />
+          <input
+            type="text"
+            name="text"
+            className="p-2 rounded-md bg-gray-100"
+          />
+          <button
+            className="mx-4 text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2"
+            type="submit"
           >
-            5m Quick Start
-          </a>
-        </li>
-        <li>
-          <a
-            className="text-blue-700 underline visited:text-purple-900"
-            target="_blank"
-            href="https://remix.run/start/tutorial"
-            rel="noreferrer"
+            Go
+          </button>
+        </Form>
+        {data?.key && <p>copy and save this url: {data?.key}</p>}
+      </div>
+      <div>
+        <h4 className="text-gray-900 py-6 font-bold text-5xl">
+          Enter your key here decrypt
+        </h4>
+        <Form method="post">
+          <input type="hidden" name="intent" value="decrypt" />
+          <input
+            type="text"
+            name="key"
+            className="p-2 rounded-md bg-gray-100"
+          />
+          <button
+            className="mx-4 text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2"
+            type="submit"
           >
-            30m Tutorial
-          </a>
-        </li>
-        <li>
-          <a
-            className="text-blue-700 underline visited:text-purple-900"
-            target="_blank"
-            href="https://remix.run/docs"
-            rel="noreferrer"
-          >
-            Remix Docs
-          </a>
-        </li>
-      </ul>
+            Go
+          </button>
+        </Form>
+        {data?.message && <p>message</p>}
+      </div>
     </div>
   );
 }
