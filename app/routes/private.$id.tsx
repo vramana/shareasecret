c1e24a21598353863506579d471f36cf0eec80e2
Ramana Venkata <vramana@users.noreply.github.com>
2024-08-19 06:46:07 +0530

    Burn the secret

--- app/routes/private.$id.tsx
+++ app/routes/private.$id.tsx
@@ -1,6 +1,6 @@
-import { type LoaderFunctionArgs } from "@remix-run/node";
-import { useLoaderData } from "@remix-run/react";
-import { getSecretPublicData } from "~/server/secret.server";
+import { redirect, type LoaderFunctionArgs } from "@remix-run/node";
+import { Form, useLoaderData } from "@remix-run/react";
+import { burnSecret, getSecretPublicData } from "~/server/secret.server";
 
 import { getSession } from "~/server/session.server";
 import { commonMeta } from "~/utils/meta";
@@ -25,22 +25,41 @@ export const loader = async ({ params, request }: LoaderFunctionArgs) => {
   return { id: params.id, secretData };
 };
 
+export const action = async ({ params }: LoaderFunctionArgs) => {
+  const { id } = params as { id: string };
+
+  await burnSecret(id);
+
+  return redirect(`/private/${id}`);
+};
+
 export default function Private() {
   const data = useLoaderData<typeof loader>();
 
   return (
     <div>
-      <h2 className="text-gray-900 py-6 font-bold text-xl">Details</h2>
-      {"secretUrl" in data && (
-        <>
-          <input
-            className="p-1 w-full"
-            disabled
-            defaultValue={data.secretUrl}
-          />
-          <input disabled defaultValue={data.secretText} />
-        </>
-      )}
+      <Form method="post" className="flex flex-col align-center">
+        <h2 className="text-gray-900 py-6 font-bold text-xl">Details</h2>
+        {"secretUrl" in data && (
+          <>
+            <input
+              className="p-1 w-full"
+              disabled
+              defaultValue={data.secretUrl}
+            />
+            <input disabled defaultValue={data.secretText} />
+          </>
+        )}
+        <button className="block my-4 text-white bg-green-600 hover:bg-green-700 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2">
+          Burn this secret
+        </button>
+      </Form>
+      <a
+        href="/"
+        className="block my-4 text-white bg-green-600 hover:bg-green-700 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2"
+      >
+        Create another secret
+      </a>
     </div>
   );
 }
--- app/routes/secret.$cuid.$key.tsx
+++ app/routes/secret.$cuid.$key.tsx
@@ -44,7 +44,9 @@ export default function Secret() {
   } else if (loaderData.exists) {
     content = (
       <Form method="post" action={pathname}>
-        <button>Show secret</button>
+        <button className="block my-4 text-white bg-green-600 hover:bg-green-700 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2">
+          Show secret
+        </button>
       </Form>
     );
   } else {
